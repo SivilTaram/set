975a924e36295e7f8d27e55e2a05a923056c8e64
fanfeilong <fanfeilong@xunlei.com>
2015-11-26 13:50:50 +0800

    refactor doc dir

---
+++ src/calculate/UnitTest/CalculateTest.cs
@@ -0,0 +1,78 @@
+﻿using calculate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for CalculateTest and is intended
+    ///to contain all CalculateTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class CalculateTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Execute
+        ///</summary>
+        [TestMethod()]
+        public void ExecuteTest()
+        {
+            Calculate calculate = new Calculate();
+            Assert.AreEqual(3, calculate.Execute("+", 1, 2));
+            Assert.AreEqual(4, calculate.Execute("-", 10, 6));
+            Assert.AreEqual(2, calculate.Execute("*", 1, 2));
+            Assert.AreEqual(2, calculate.Execute("/", 4, 2));
+        }
+    }
+}
---
+++ src/calculate/calculate/Calculate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace calculate
+{
+    public class Calculate
+    {
+        public int Execute(string op, int a, int b)
+        {
+            int value = 0;
+            switch (op)
+            {
+                case "+":
+                    value = a + b;
+                    break;
+                case "-":
+                    value = a - b;
+                    break;
+                case "*":
+                    value = a * b;
+                    break;
+                case "/":
+                    value = a / b;
+                    break;
+                default:
+                    break;
+            }
+            return value;
+        }
+    }
+}
